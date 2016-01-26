a8e70811e2f6b8cf019ce4ce4ebc57157d319d12
shenyongkang <kang200724001@gmail.com>
2016-01-26 22:05:56 +0800

    different router strategy

--- src/pacman/entries/ghosts/MyGhosts.java
+++ src/pacman/entries/ghosts/MyGhosts.java
@@ -4,6 +4,7 @@ import java.util.EnumMap;
 import java.util.Random;
 
 import pacman.controllers.Controller;
+import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
 import pacman.game.Game;
@@ -13,22 +14,61 @@ import pacman.game.Game;
  * fill in the getActions() method. Any additional classes you write should either
  * be placed in this package or sub-packages (e.g., game.entries.ghosts.mypackage).
  */
-public class MyGhosts extends Controller<EnumMap<GHOST,MOVE>>
-{
-	private EnumMap<GHOST, MOVE> myMoves=new EnumMap<GHOST, MOVE>(GHOST.class);
-	private MOVE[] allMoves=MOVE.values();
-	private Random rnd=new Random();
-	public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue)
+public final class MyGhosts extends Controller<EnumMap<GHOST,MOVE>>
+{	
+	private final static float CONSISTENCY=0.95f;	//attack Ms Pac-Man with this probability
+	private final static int PILL_PROXIMITY=15;		//if Ms Pac-Man is this close to a power pill, back away
+	
+	Random rnd=new Random();
+	EnumMap<GHOST,MOVE> myMoves=new EnumMap<GHOST,MOVE>(GHOST.class);
+	
+	public EnumMap<GHOST,MOVE> getMove(Game game,long timeDue)
 	{
-		myMoves.clear();
-		
-		//Place your game logic here to play the game as the ghosts
-		
-		
-		for(GHOST ghostType : GHOST.values())
-			if(game.doesGhostRequireAction(ghostType))
-				myMoves.put(ghostType,allMoves[rnd.nextInt(allMoves.length)]);
-		
+		for(GHOST ghost : GHOST.values())	//for each ghost
+		{			
+			DM mode = DM.PATH;
+			if (ghost == GHOST.BLINKY){
+				mode = DM.EUCLID;
+				
+			}else if(ghost == GHOST.INKY){
+				mode = DM.MANHATTAN;
+				
+			}else if (ghost == GHOST.PINKY){
+				mode = DM.PATH;
+			}else{
+				
+			}
+			if(game.doesGhostRequireAction(ghost))		//if ghost requires an action
+			{
+				if(game.getGhostEdibleTime(ghost)>0 || closeToPower(game))	//retreat from Ms Pac-Man if edible or if Ms Pac-Man is close to power pill
+					myMoves.put(ghost,game.getApproximateNextMoveAwayFromTarget(game.getGhostCurrentNodeIndex(ghost),
+							game.getPacmanCurrentNodeIndex(),game.getGhostLastMoveMade(ghost),mode));
+				else 
+				{
+					if(rnd.nextFloat()<CONSISTENCY)			//attack Ms Pac-Man otherwise (with certain probability)
+						myMoves.put(ghost,game.getApproximateNextMoveTowardsTarget(game.getGhostCurrentNodeIndex(ghost),
+								game.getPacmanCurrentNodeIndex(),game.getGhostLastMoveMade(ghost),mode));
+					else									//else take a random legal action (to be less predictable)
+					{					
+						MOVE[] possibleMoves=game.getPossibleMoves(game.getGhostCurrentNodeIndex(ghost),game.getGhostLastMoveMade(ghost));
+						myMoves.put(ghost,possibleMoves[rnd.nextInt(possibleMoves.length)]);
+					}
+				}
+			}
+		}
+
 		return myMoves;
 	}
+	
+    //This helper function checks if Ms Pac-Man is close to an available power pill
+	private boolean closeToPower(Game game)
+    {
+    	int[] powerPills=game.getPowerPillIndices();
+    	
+    	for(int i=0;i<powerPills.length;i++)
+    		if(game.isPowerPillStillAvailable(i) && game.getShortestPathDistance(powerPills[i],game.getPacmanCurrentNodeIndex())<PILL_PROXIMITY)
+    			return true;
+
+        return false;
+    }
 }
